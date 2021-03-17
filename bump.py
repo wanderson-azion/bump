744f6d500a99db6dfcac12be70168c1441762b1f
Wanderson Camargo <wanderson.camargo@azion.com>
2021-03-17 10:29:52 -0300

    Teste

---
+++ bump.py
@@ -0,0 +1,25 @@
+import re
+import subprocess
+
+version = subprocess.check_output(["git", "describe", "--tags", "--abbrev=0"]).strip()
+message = subprocess.check_output(["git", "log", "--pretty=format:%s", "--max-count=1"]).strip()
+
+pattern_version = "^(\d+)\.(\d+)\.(\d+)$"
+pattern_message = "^Merge pull request #\d+ from (.*feat.*)$"
+
+if not re.match(pattern_version, version):
+    print("1.0.0")
+    exit()
+
+result = re.search(pattern_version, version)
+
+major = int(result.group(1))
+minor = int(result.group(2))
+patch = int(result.group(3))
+
+if re.match(pattern_message, message):
+    minor += 1
+else:
+    patch += 1
+
+print("{}.{}.{}".format(major, minor, patch))
